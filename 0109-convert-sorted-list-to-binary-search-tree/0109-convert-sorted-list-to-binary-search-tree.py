17345cb7378f3472356e42867d6a894d222dd992
Jillella Susmitha <jillellasusmitha24@gmail.com>
2026-08-26 18:58:57 +0530

    Time: 6 ms (51.91%), Space: 21.4 MB (79.75%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
@@ -10,26 +10,40 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.arr = []
-    def convert(self,head):
-        temp = head
-        while temp:
-            self.arr.append(temp.val)
-            temp = temp.next
-        # print(self.arr)
-        return self.arr
-    def intoBST(self,l,r,arr):
-        if l > r:
-            return
-        mid = (l+r)//2
-        root = TreeNode(arr[mid])
-        root.left = self.intoBST(l,mid-1,arr)
-        root.right = self.intoBST(mid+1,r,arr)
-        return root
+    # def __init__(self):
+    #     self.arr = []
+    # def convert(self,head):
+    #     temp = head
+    #     while temp:
+    #         self.arr.append(temp.val)
+    #         temp = temp.next
+    #     # print(self.arr)
+    #     return self.arr
+    # def intoBST(self,l,r,arr):
+    #     if l > r:
+    #         return
+    #     mid = (l+r)//2
+    #     root = TreeNode(arr[mid])
+    #     root.left = self.intoBST(l,mid-1,arr)
+    #     root.right = self.intoBST(mid+1,r,arr)
+    #     return root
 
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        self.convert(head)
+        # self.convert(head)
         # print(self.arr)
-        return self.intoBST(0,len(self.arr)-1,self.arr)
-        
+        # return self.intoBST(0,len(self.arr)-1,self.arr)
+
+        if not head:
+            return None
+        if not head.next:
+            return TreeNode(head.val)
+        slow = head;slowPrev = None;fast = head
+        while fast and fast.next:
+            slowPrev = slow
+            slow = slow.next
+            fast = fast.next.next
+        slowPrev.next = None
+        root = TreeNode(slow.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(slow.next)
+        return root
